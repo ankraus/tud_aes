23748aef1f8df5f3a1935152f4310cfa9caf0296
Andreas Kraus <andreas.kraus.97@gmail.com>
2024-04-08 12:43:21 +0100

    refactor tests so that they are executed three times with random buffers

--- rijndael_test.py
+++ rijndael_test.py
@@ -40,69 +40,76 @@ def c_buffers_match(old, new):
 
 
 def test_sub_bytes():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    rijndael.sub_bytes(c_buffer)
-    sub_bytes(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        rijndael.sub_bytes(c_buffer)
+        sub_bytes(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
 
 
 def test_inv_sub_bytes():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    rijndael.sub_bytes(c_buffer)
-    sub_bytes(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
-    rijndael.invert_sub_bytes(c_buffer)
-    inv_sub_bytes(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        rijndael.sub_bytes(c_buffer)
+        sub_bytes(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
+        rijndael.invert_sub_bytes(c_buffer)
+        inv_sub_bytes(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert c_buffers_match(original_c_buffer, c_buffer)
 
 
 def test_shift_rows():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    rijndael.shift_rows(c_buffer)
-    shift_rows(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        rijndael.shift_rows(c_buffer)
+        shift_rows(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
 
 
 def test_inv_shift_rows():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    rijndael.shift_rows(c_buffer)
-    shift_rows(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
-    rijndael.invert_shift_rows(c_buffer)
-    inv_shift_rows(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        rijndael.shift_rows(c_buffer)
+        shift_rows(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
+        rijndael.invert_shift_rows(c_buffer)
+        inv_shift_rows(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert c_buffers_match(original_c_buffer, c_buffer)
 
 
 def test_mix_columns():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    rijndael.mix_columns(c_buffer)
-    mix_columns(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        rijndael.mix_columns(c_buffer)
+        mix_columns(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
 
 
 def test_inv_mix_columns():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    rijndael.mix_columns(c_buffer)
-    mix_columns(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
-    rijndael.invert_mix_columns(c_buffer)
-    inv_mix_columns(py_buffer)
-    assert buffers_match(c_buffer, py_buffer)
-    assert c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        rijndael.mix_columns(c_buffer)
+        mix_columns(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
+        rijndael.invert_mix_columns(c_buffer)
+        inv_mix_columns(py_buffer)
+        assert buffers_match(c_buffer, py_buffer)
+        assert c_buffers_match(original_c_buffer, c_buffer)
 
 
 def test_add_round_key():
-    c_buffer, py_buffer, original_c_buffer = gen_buffers()
-    c_key, py_key = gen_keys()
-    rijndael.add_round_key(c_buffer, c_key)
-    add_round_key(py_buffer, py_key)
-    assert buffers_match(c_buffer, py_buffer)
-    assert not c_buffers_match(original_c_buffer, c_buffer)
+    for _ in range(3):
+        c_buffer, py_buffer, original_c_buffer = gen_buffers()
+        c_key, py_key = gen_keys()
+        rijndael.add_round_key(c_buffer, c_key)
+        add_round_key(py_buffer, py_key)
+        assert buffers_match(c_buffer, py_buffer)
+        assert not c_buffers_match(original_c_buffer, c_buffer)
